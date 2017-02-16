a515442155863dcd4b811b99a33a4e51a50c4013
Ryan Bozarth <ryan_bozarth@ryan-bozarth.lan>
2017-02-15 20:26:20 -0800

    added actions and reducers from lesson

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 require('babel-polyfill');
 
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import * as actions from '../actions/index';
+import repositoryReducer from '../reducers/index';
