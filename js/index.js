e961144bec454b8f8a2087e40003047ca951f1a7
Ryan Bozarth <ryan_bozarth@ryan-bozarth.local>
2017-02-16 15:22:43 -0800

    created store

--- js/index.js
+++ js/index.js
@@ -3,5 +3,8 @@ require('babel-polyfill');
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import * as actions from '../actions/index';
-import repositoryReducer from '../reducers/index';
+import * as actions from './actions/index';
+import * as reducers from './reducers/index';
+
+// const addRepo = addRepository(repository);
+// const newState = repositoryReducer(state, addRepo);
