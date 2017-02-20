c40480561a8ed394f7db1ade3f3ff1902da69e43
Ryan Bozarth <ryan_bozarth@ryan-bozarth.lan>
2017-02-20 13:46:59 -0800

    adding components

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 require('babel-polyfill');
 
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 
-import * as actions from './actions/index';
-import * as reducers from './reducers/index';
+import store from './store';
+import RepositoryList from './components/repository-list';
 
-// const addRepo = addRepository(repository);
-// const newState = repositoryReducer(state, addRepo);
+document.addEventListener('DOMContentLoaded', () =>
+    ReactDOM.render(
+        <Provider store={store}>
+            <RepositoryList />
+        </Provider>,
+        document.getElementById('app')
+    )
+);
